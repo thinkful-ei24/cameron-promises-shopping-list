cdf9d4d1a1b4bbb32113a35ad995917ec34507fc
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-29 11:28:48 -0500

    added findAndUpdate to store.js

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 /* global Item */
-
+'use strict';
 // eslint-disable-next-line no-unused-vars
 const store = (function(){
   const addItem = function(item) {
@@ -8,27 +8,16 @@ const store = (function(){
     
   };
 
+  const findAndUpdate = function (id, newData){
+    Object.assign(this.findById(id), newData);
+  };
+
   const findById = function(id) {
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
-
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
-  };
-
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
   };
 
   const toggleCheckedFilter = function() {
@@ -43,14 +32,12 @@ const store = (function(){
     items: [],
     hideCheckedItems: false,
     searchTerm: '',
-
     addItem,
     findById,
-    findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
+    findAndUpdate
   };
   
 }());
