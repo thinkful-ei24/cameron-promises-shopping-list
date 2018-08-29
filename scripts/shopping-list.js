9b4498917a21b0fa6271806a7d5685d8710cd513
Janet Leon <leon.irenejanet@gmail.com>
2018-08-29 11:09:27 -0700

    completed handleShoppingItemSubmit and handleItemCheckClicked

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -80,8 +80,12 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      store.findAndToggleChecked(id);
-      render();
+      const currentItem = store.findById(id);
+      api.updateItem(id, {checked: !currentItem.checked}, function(){
+        store.findAndUpdate(id, {checked: !currentItem.checked});
+        render();
+      }
+      ); 
     });
   }
   
@@ -102,8 +106,12 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      store.findAndUpdateName(id, itemName);
-      render();
+      api.updateItem(id, {name : itemName}, function(){
+        store.findAndUpdate(id, {name: itemName});
+        render();
+      }
+      );
+    
     });
   }
   
